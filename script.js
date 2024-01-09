94ee7ae00a83d028270531b112a2919104b1b784
SohyangO <shyang903@naver.com>
2024-01-09 15:59:07 +0900

    css 수정

--- script.js
+++ script.js
@@ -1,80 +1,75 @@
 const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNmI2ZGJkM2YxNTIwYzU0Mzk4OGMxZWQ3ZTIwNTUyZCIsInN1YiI6IjY1OTYxYjkxNTkwN2RlNjYyNTYzYzAwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p8IC4h9MKHXEqeoBrSxdS0xHXBWkwoCiNYdn9Xf1HZ4'
-    }
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNmI2ZGJkM2YxNTIwYzU0Mzk4OGMxZWQ3ZTIwNTUyZCIsInN1YiI6IjY1OTYxYjkxNTkwN2RlNjYyNTYzYzAwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p8IC4h9MKHXEqeoBrSxdS0xHXBWkwoCiNYdn9Xf1HZ4"
+  }
 };
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(response => {
-        function displayMovies() { // 기본 로딩 화면
-            response.results.forEach((movie) => {
-                const movieDiv = document.getElementById("movie");
-                movieDiv.innerHTML += `
-                <div class="movieCard" movieId="${movie.id}">
+fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", options)
+  .then((response) => response.json())
+  .then((response) => {
+    function displayMovies() {
+      // 기본 로딩 화면
+      response.results.forEach((movie) => {
+        const movieDiv = document.getElementById("movie");
+        movieDiv.innerHTML += `
+                <li class="movieCard" movieId="${movie.id}">
                 <h2>${movie.title}</h2>
                 <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="">
                 <p>${movie.overview}</p>
                 <p>Ratings ${movie.vote_average}/10</p>
-        </div>`})
-            /*-------------카드 클릭 시 alert창 띄우기-----------------*/
-            const movieAll= document.querySelectorAll(".movieCard")
-            movieAll.forEach( movie =>
-                movie.addEventListener('click', clickBox));
+                </li>`;
+      });
+      /*-------------카드 클릭 시 alert창 띄우기-----------------*/
+      const movieAll = document.querySelectorAll(".movieCard");
+      movieAll.forEach((movie) => movie.addEventListener("click", clickBox));
 
-            function clickBox(event) {
-                alert(`Movie ID: ${event.currentTarget.getAttribute('movieId')}`);
-            }
-        }
-        displayMovies();
+      function clickBox(event) {
+        alert(`Movie ID: ${event.currentTarget.getAttribute("movieId")}`);
+      }
+    }
 
-        console.log(response.results);
+    displayMovies();
 
+    console.log(response.results);
 
+    /*-----검색 시 이벤트 발생-----*/
 
-        /*-----검색 시 이벤트 발생-----*/
+    document.getElementById("btn").addEventListener("click", surf); //검색 버튼 클릭 시, 데이터 찾기 실행
+    function surf() {
+      //실행할 함수
+      fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", options)
+        .then((response) => response.json())
+        .then((response) => {
+          const surfTerm = document.getElementById("surfInput").value.trim().toUpperCase(); //검색창 입력값 받아옴
+          const movieDiv2 = document.getElementById("movie"); //받아온 데이터 출력할 div
+          const filtered = response.results.filter((movie) => movie.title.toUpperCase().includes(surfTerm)); //필터기능
+          movieDiv2.innerHTML = "";
 
-        document.getElementById("btn").addEventListener("click", surf); //검색 버튼 클릭 시, 데이터 찾기 실행
-        function surf() { //실행할 함수
-            fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-                .then(response => response.json())
-                .then(response => {
-                    const surfTerm = document.getElementById("surfInput").value.trim().toUpperCase(); //검색창 입력값 받아옴
-                    const movieDiv2 = document.getElementById("movie"); //받아온 데이터 출력할 div
-                    const filtered = response.results.filter(movie =>
-                        movie.title.toUpperCase().includes(surfTerm)); //필터기능
-                    movieDiv2.innerHTML = "";
-
-                    filtered.forEach(movie => {
-                        movieDiv2.innerHTML += `
-                            <div class="movieCard" movieId="${movie.id}">
+          filtered.forEach((movie) => {
+            movieDiv2.innerHTML += `
+                            <li class="movieCard" movieId="${movie.id}">
                              <h2>${movie.title}</h2>
                              <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="">
                              <p>${movie.overview}</p>
                              <p>Ratings ${movie.vote_average}/10</p>
-                             </div>`
-                    })
+                             </li>`;
+          });
 
-                    if (!surfTerm) { //input창에 아무것도 없는 경우, 경고창 띄우기
-                        alert("Please enter a movie title.");
-                        document.getElementById("surfInput").focus();
-                        return false;
-                    }
-                    const movieAll= document.querySelectorAll(".movieCard")
-                    movieAll.forEach( movie =>
-                        movie.addEventListener('click', clickBox));
-        
-                    function clickBox(event) {
-                        alert(`Movie ID: ${event.currentTarget.getAttribute('movieId')}`);
-                    }
-                });
-        }
+          if (!surfTerm) {
+            //input창에 아무것도 없는 경우, 경고창 띄우기
+            alert("Please enter a movie title.");
+            document.getElementById("surfInput").focus();
+          }
 
-        //  function enterkey() {
-        //     if (e.key == "Enter" || e.keyCode == 13){
-        //         surf();
-        //     }          
-        // }
-    })
-    .catch(err => console.error(err));
+          const movieAll = document.querySelectorAll(".movieCard");
+          movieAll.forEach((movie) => movie.addEventListener("click", clickBox));
+
+          function clickBox(event) {
+            alert(`Movie ID: ${event.currentTarget.getAttribute("movieId")}`);
+          }
+        });
+    }
+  })
+  .catch((err) => console.error(err));
