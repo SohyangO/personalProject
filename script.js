11aaa8d1143a9582e48fb9a4c5f0c9b03edb6735
SohyangO <shyang903@naver.com>
2024-01-09 16:56:56 +0900

    엔터키 입력 기능 추가

--- script.js
+++ script.js
@@ -37,6 +37,13 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", opti
     /*-----검색 시 이벤트 발생-----*/
 
     document.getElementById("btn").addEventListener("click", surf); //검색 버튼 클릭 시, 데이터 찾기 실행
+    surfInput.addEventListener("keydown", ({ key }) => {
+      //엔터키로 검색어 입력 시, 데이터 찾기 실행
+      if (key !== "Enter") {
+        return;
+      }
+      surf();
+    });
     function surf() {
       //실행할 함수
       fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", options)
@@ -45,6 +52,7 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", opti
           const surfTerm = document.getElementById("surfInput").value.trim().toUpperCase(); //검색창 입력값 받아옴
           const movieDiv2 = document.getElementById("movie"); //받아온 데이터 출력할 div
           const filtered = response.results.filter((movie) => movie.title.toUpperCase().includes(surfTerm)); //필터기능
+          console.log(filtered);
           movieDiv2.innerHTML = "";
 
           filtered.forEach((movie) => {
@@ -60,6 +68,11 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", opti
           if (!surfTerm) {
             //input창에 아무것도 없는 경우, 경고창 띄우기
             alert("Please enter a movie title.");
+            document.getElementById("surfInput").focus();
+          }
+
+          if (filtered.length === 0) {
+            alert(`Sorry! Not matching search keywords in this page.\n  Please enter another movie title.`);
             document.getElementById("surfInput").focus();
           }
 
